aa4971b427ec1fcbbfd03b555c75a1d821dbde53
phunh2704 <phunh2704@gmail.com>
2025-03-21 02:06:07 +0700

    update datatable

--- computer/models.py
+++ computer/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here.
 class Computer(models.Model):
     pcname = models.CharField(max_length=10, null=False)
-    username = models.CharField(max_length=10)
-    userid = models.CharField(max_length=10)
-    name = models.CharField(max_length=40)
-    job = models.CharField(max_length=40)
-    department = models.CharField(max_length=40)
-    note = models.CharField(max_length=40)
+    username = models.CharField(max_length=10,null=True)
+    userid = models.CharField(max_length=10,null=True)
+    name = models.CharField(max_length=40,null=True)
+    job = models.CharField(max_length=40,null=True)
+    department = models.CharField(max_length=40,null=True)
+    note = models.CharField(max_length=40,null=True)
     class Meta:
         db_table = 'computer'
